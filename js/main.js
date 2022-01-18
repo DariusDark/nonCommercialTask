76ec945c58859733c3d6a717d82eda1a1dba422b
Darius <darius99dark@mail.ru>
2022-01-18 05:56:46 +0500

    pre-final version

--- js/main.js
+++ js/main.js
@@ -1,24 +1,47 @@
-document.addEventListener('touchstart', touchStart, false)
-const track = document.getElementById('track');
-const area = document.body.offsetWidth;
+const descriptionContainer = document.getElementById('descriptionContainer');
+const descriptionScroll = document.getElementById('descriptionScroll');
+const modalScreenSlider = document.getElementById('modalScreenSlider');
+const thingsSecondPage = document.getElementById('thingsSecondPage');
+const modalScreenDots = document.getElementById('modalScreenDots');
+const btnCloseModal = document.getElementById('closeModal');
+const modalScreen = document.getElementById('modalScreen');
+const btnOpenModal = document.getElementById('openModal');
 const btnForward = document.getElementById('btnForward');
+const scrollItem = document.getElementById('scrollItem');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
 const btnHome = document.getElementById('home');
+const track = document.getElementById('track');
 
+const area = document.body.offsetWidth;
 
-let clientTouchX; 
+let clientTouchX;
 let swiperCount = 0;
+
+descriptionScroll.addEventListener('scroll', function () {
+    const scrollTop = this.scrollTop;
+
+    descriptionContainer.scrollTop = scrollTop;
+});
 
 btnForward.addEventListener('click', () => handleClick(1), false);
 btnHome.addEventListener('click', () => handleClick(0), false);
 
+document.addEventListener('touchstart', touchStart, false)
+
+
 function handleClick(value) {
     swiperCount = value;
-    track.style.transform = `translateX(-${swiperCount * area}px)`;
+
+    if (modalScreen.classList.contains('active')) modalScreen.classList.remove('active');
+
+    changeTrackPos(swiperCount);
 }
 
 function touchStart(event) {
     clientTouchX = event.touches['0'].clientX;
     document.addEventListener('touchmove', touchMove, false)
+
 }
 
 function touchMove(event) {
@@ -26,13 +49,18 @@ function touchMove(event) {
     if (clientMoveX > clientTouchX) {
         decrementValue(swiperCount);
 
-        track.style.transform = `translateX(-${swiperCount * area}px)`;
+        changeTrackPos(swiperCount);
     } else {
         incrementValue(swiperCount);
-        
-        track.style.transform = `translateX(-${swiperCount * area}px)`;
+
+        changeTrackPos(swiperCount);
     }
+
     document.removeEventListener('touchmove', touchMove);
+}
+
+function changeTrackPos(value) {
+    track.style.transform = `translateX(-${value * area}px)`;
 }
 
 function decrementValue(value) {
@@ -42,3 +70,59 @@ function decrementValue(value) {
 function incrementValue(value) {
     if (value >= 0 && value < 2) ++swiperCount
 }
+
+function modalScreenInit() {
+    modalScreen.addEventListener('click', function (event) {
+        if (this === event.target) this.classList.remove('active');
+    });
+
+    btnCloseModal.addEventListener('click', () => {
+        modalScreen.classList.remove('active');
+    });
+
+    btnOpenModal.addEventListener('click', () => {
+        modalScreen.classList.add('active');
+    }, false);
+}
+
+function sliderInit() {
+    let i = 0;
+
+    prevBtn.addEventListener('click', () => {
+        if (i <= 0) {
+            i = 0;
+        } else {
+            modalScreenSlider.children[i].classList.remove('show');
+            modalScreenDots.children[i].checked = false;
+            modalScreenSlider.children[--i].classList.add('show');
+            modalScreenDots.children[i].checked = true;
+        }
+    })
+
+    nextBtn.addEventListener('click', () => {
+        if (i >= 1) {
+            i = 1
+        } else {
+            modalScreenSlider.children[i].classList.remove('show');
+            modalScreenDots.children[i].checked = false;
+            modalScreenSlider.children[++i].classList.add('show');
+            modalScreenDots.children[i].checked = true;
+        }
+    })
+}
+
+sliderInit();
+modalScreenInit();
+
+setTimeout(() => {
+    scrollItem.style.height = descriptionContainer.scrollHeight + 'px';
+    descriptionScroll.style.height = descriptionContainer.offsetHeight + 'px';
+}, 0)
+
+setInterval(() => {
+    if (swiperCount >= 1) {
+        thingsSecondPage.classList.add('active');
+    } else {
+        thingsSecondPage.classList.remove('active');
+    }
+}, 100)
